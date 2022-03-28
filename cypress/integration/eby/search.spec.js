6a004fdb38c5cd19d5b4773b650e55b37f7023cc
rchovatiya88 <rchovatiya88@gmail.com>
2022-03-27 23:04:33 -0400

    stuff

---
+++ cypress/integration/eby/search.spec.js
@@ -0,0 +1,16 @@
+describe('Go to Twitter and Login', () => {
+    it('Homepage', () => {
+        cy.visit('https://www.ebay.com/');
+
+    });
+
+    it('search for stuff', () => {
+
+        cy.findByText('').click();
+        cy.get('').click();
+    });
+
+
+
+
+});
