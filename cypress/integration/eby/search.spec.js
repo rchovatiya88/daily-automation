0e81ffcbc52ded0217306e075de027450b682677
rchovatiya88 <rchovatiya88@gmail.com>
2022-03-28 00:34:53 -0400

    smile your on camera

--- cypress/integration/eby/search.spec.js
+++ cypress/integration/eby/search.spec.js
@@ -1,12 +1,12 @@
 describe('Go to eban search for mens jewelry', () => {
-    const pageNum = 1;
+    const pageNum = 1 + Math.floor(Math.random() * 10);
     it('Homepage', () => {
         cy.visit('https://www.ebay.com/b/Mens-Jewelry/10290?Color=Gold&Main%2520Stone=Diamond&Metal=Yellow%2520Gold&Metal%2520Purity=14k&LH_BIN=1&Type=Necklace%7CRing%7CPendant&rt=nc&_udlo=150&_udhi=1%2C500&mag=' + pageNum);
 
     });
 
     it('wait for stuff', () => {
-        cy.wait(5000);
+        cy.wait(15000);
         // cy.get('#gh-ac').type("gold chain with pendant").type('{enter}');
         // cy.get('#gh-btn').click();
     });
